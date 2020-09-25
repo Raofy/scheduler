2137ad84eac43c9cb6c1e73391145d5aa3cda056
富沂 <1312170600@qq.com>
2020-09-25 16:58:20 +0800

    完成xxl-job调度器demo

--- xxl-job/src/main/java/com/ryan/xxljob/XxlJobApplication.java
+++ xxl-job/src/main/java/com/ryan/xxljob/XxlJobApplication.java
@@ -2,12 +2,19 @@ package com.ryan.xxljob;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
 @SpringBootApplication
+@EnableScheduling
 public class XxlJobApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(XxlJobApplication.class, args);
     }
 
+    @Scheduled(fixedRate=10000)
+    public void init(){
+        System.out.println("init>>>");
+    }
 }
